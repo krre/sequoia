88baf82591404b58bb8d2865fa3af73cfd926783
Vladimir Zarypov <krre31@gmail.com>
2015-04-14 08:40:29 +0300

    added creating new repository

--- cpp/repository.cpp
+++ cpp/repository.cpp
@@ -7,7 +7,13 @@ Repository::Repository(QObject *parent) : QObject(parent)
 
 void Repository::create(QUrl filepath)
 {
-    QDir().mkpath(filepath.toLocalFile() + ".sequoia");
+    QString sourcePath = filepath.toLocalFile();
+    QString repoPath = sourcePath + ".sequoia";
+    bool result = QDir().mkpath(repoPath);
+    if (result) {
+        QString targetPath = repoPath + QDir::separator() + filepath.fileName();
+        QFile::copy(sourcePath, targetPath);
+    }
 }
 
 
